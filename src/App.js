4ababb380d7cf5c1528d97d533c049dc9c1f63de
[hamza-boudefa] <boudefahamza@gmail.com>
2022-06-02 19:11:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Sidebar from './SideBar';
+import Video from './Video';
+
+function App() {
+
+  return (
+    <div className="App">
+    <Header/>
+    <Sidebar/>
+    {/* <Video/> */}
+    </div>
+  );
+}
+export default App;
